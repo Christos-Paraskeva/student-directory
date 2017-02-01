da16f82771916a81241da4f40bc56a659067e85b
Christos <chris.paraskeva10@gmail.com>
2017-02-01 19:39:43 +0000

    added load from file feature

--- directory.rb
+++ directory.rb
@@ -151,6 +151,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -169,6 +170,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit # this will cause the program to terminate
   else
@@ -188,6 +191,14 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
 # def print_filtered_name_length(students)
 #   puts "Would you like to filter the list based on the length of the name?"
 #   puts "If so, then please enter the max length the name can be?"
